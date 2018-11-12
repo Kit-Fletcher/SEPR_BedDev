e4457180c4fb99278c89337a2bce5154a0ca1ebc
Kit-Fletcher <44055670+Kit-Fletcher@users.noreply.github.com>
2018-11-12 15:45:26 +0000

    Add files via upload

---
+++ project/SEPR_Project/desktop/src/com/sepr/project/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sepr.project.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sepr.project.SEPRProject;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SEPRProject(), config);
+	}
+}
