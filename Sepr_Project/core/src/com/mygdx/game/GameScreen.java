9c43ba45b4bae35297198c30bc947a6bc680e527
eddiebattman <eddie_batt@hotmail.com>
2018-11-30 16:47:14 +0000

    Added isKeyPressed. Takes a keys value as input and checks if it is currently being pressed.

--- Sepr_Project/core/src/com/mygdx/game/GameScreen.java
+++ Sepr_Project/core/src/com/mygdx/game/GameScreen.java
@@ -5,6 +5,19 @@ import com.badlogic.gdx.Input;
 
 public class GameScreen {
 	
+	/**
+	 * Checks if the key is pressed
+	 * @param key the value of the key you want to check;use Input.keys.'keyname' to get desired keycode
+	 * @return Boolean value
+	 */
+	public Boolean isKeyPressed(int key) {
+		if(Gdx.input.isKeyPressed(key)){
+			return true;
+			
+		}else {
+			return false;
+		}
+	}
 	
 	public Integer getMouseCoordinatesX() {
 		return Gdx.input.getX();
