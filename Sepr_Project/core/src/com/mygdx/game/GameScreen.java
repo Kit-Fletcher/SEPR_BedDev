c8a861f399e2b7a9aacced30578b422141ad1d8d
eddiebattman <eddie_batt@hotmail.com>
2018-11-30 16:42:43 +0000

    Added mouse coordinate getters.

--- Sepr_Project/core/src/com/mygdx/game/GameScreen.java
+++ Sepr_Project/core/src/com/mygdx/game/GameScreen.java
@@ -1,5 +1,20 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+
 public class GameScreen {
+	
+	
+	public Integer getMouseCoordinatesX() {
+		return Gdx.input.getX();
+	}
+	
+	public Integer getMouseCoordinatesY() {
+		return Gdx.input.getY();
+	}
+	
+	
+	
 
 }
