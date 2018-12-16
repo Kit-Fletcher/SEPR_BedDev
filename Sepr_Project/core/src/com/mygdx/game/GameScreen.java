104a09996377e8327c1e9450093cc06f50484c3f
eddiebattman <eddie_batt@hotmail.com>
2018-12-16 13:24:54 +0000

    Moved movement to player and added getter

--- Sepr_Project/core/src/com/mygdx/game/GameScreen.java
+++ Sepr_Project/core/src/com/mygdx/game/GameScreen.java
@@ -17,6 +17,10 @@ public class GameScreen {
 		controls.put("RIGHT", Input.Keys.D);
 	
 	}
+	
+	public HashMap<String, Integer> getControls(){
+		return controls;
+	}
 	/**
 	 * Checks if the key is pressed
 	 * @param key the value of the key you want to check;use Input.keys.'keyname' to get desired keycode
@@ -30,45 +34,7 @@ public class GameScreen {
 			return false;
 		}
 	}
-	/**
-	 * Checks movement keys for inputs and updates the character sprite
-	 * Makes sure sprite doesn't go outside of the current screen
-	 * @param character The character sprite
-	 * @param screen Current screens sprite
-	 * @return character updated with new position
-	 */
-	public Sprite getMovement(Sprite character, Sprite screen) {
-	    
-		if(Gdx.input.isKeyPressed(controls.get("LEFT"))){
-			
-            character.translateX(-5f);
-            if(character.getX() < screen.getX()) {
-            	character.setPosition(screen.getX(), character.getY());
-            }
-        }
-        if(Gdx.input.isKeyPressed(controls.get("RIGHT"))){
-        	character.translateX(5f);
-            if((character.getX() + (character.getWidth() * character.getScaleX())) > (screen.getX()+screen.getWidth())) {
-            	character.setPosition(screen.getX()+screen.getWidth()- (character.getWidth() * character.getScaleX()), character.getY());
-            }
-        }
-        if(Gdx.input.isKeyPressed(controls.get("UP"))){
-        	character.translateY(5f);
-            if(character.getY() + (character.getHeight() * character.getScaleY()) > (screen.getY() + screen.getHeight())) {
-                character.setPosition(character.getX(), (screen.getY() + screen.getHeight()- (character.getHeight() * character.getScaleY())));	
-            }
-        }
-        if(Gdx.input.isKeyPressed(controls.get("DOWN"))){
-             character.translateY(-5f);
-             if(character.getY() < screen.getY()) {
-               	character.setPosition(character.getX(), screen.getY());
-             }
-               
-        }
-		return character;
-		
-		
-	}
+	
 	// note that mouse coordinates are measured from top left of screen and sprites from bottom left
 	public Integer getMouseCoordinatesX() {
 		return Gdx.input.getX();
