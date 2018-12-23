e91f0b36da77055681741e3492834fe046a11b91
eddiebattman <eddie_batt@hotmail.com>
2018-12-23 17:34:31 +0000

    Changed border detection

--- Sepr_Project/core/src/com/mygdx/game/Player.java
+++ Sepr_Project/core/src/com/mygdx/game/Player.java
@@ -25,35 +25,21 @@ public class Player extends Characters {
 	 * @param controls The current controls map
 	 * @return sprite updated with new position
 	 */
-	// change speed to constant etc.
 	public void getMovement(Sprite screen, HashMap<String, Integer> controls) {
 	    
-		if(Gdx.input.isKeyPressed(controls.get("LEFT"))){
-			
-            sprite.translateX(-speed);
-            if(sprite.getX() + hitBoxDim[0]*scale < screen.getX()) {
-            	sprite.setPosition(screen.getX()-hitBoxDim[0]*scale , sprite.getY());
-            }
+		if(Gdx.input.isKeyPressed(controls.get("LEFT"))) {
+            sprite.translateX(-speed);      
         }
         if(Gdx.input.isKeyPressed(controls.get("RIGHT"))){
-        	sprite.translateX(speed);
-            if((sprite.getX() + hitBoxDim[1]*scale) > (screen.getX()+screen.getWidth())) {
-            	sprite.setPosition(screen.getX()+screen.getWidth() - hitBoxDim[1]*scale, sprite.getY());
-            }
+        	sprite.translateX(speed);    
         }
         if(Gdx.input.isKeyPressed(controls.get("UP"))){
-        	sprite.translateY(speed);
-            if(sprite.getY() + hitBoxDim[3]*scale > (screen.getY() + screen.getHeight())) {
-                sprite.setPosition(sprite.getX(), (screen.getY() + screen.getHeight()- hitBoxDim[3]*scale));	
-            }
+        	sprite.translateY(speed);    
         }
         if(Gdx.input.isKeyPressed(controls.get("DOWN"))){
-             sprite.translateY(-speed);
-             if(sprite.getY()+ hitBoxDim[2]*scale < screen.getY()) {
-               	sprite.setPosition(sprite.getX(), screen.getY() - hitBoxDim[2] * scale);
-             }
-               
+             sprite.translateY(-speed);       
         }
+        checkBounds(screen);
 	}
 	// checks if it is touching a zombie, currently centre of zombie has to be in its hitbox can change
 	public Boolean isTouchingZom(Zombies zombie) {
