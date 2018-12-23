8251e4b233dcde185e0cfd83d213e37dc9b2ec7e
aa1764 <44054100+aa1764@users.noreply.github.com>
2018-12-23 13:30:34 +0000

    Update SeprProject.java

--- Sepr_Project_New/Sepr_Project/core/src/com/mygdx/game/SeprProject.java
+++ Sepr_Project_New/Sepr_Project/core/src/com/mygdx/game/SeprProject.java
@@ -1,33 +1,82 @@
 package com.mygdx.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 
-public class SeprProject extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class Item extends Sprite{
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(0, 1, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
+    private String type;
+    private int id;
+    private Sprite image;
+
+
+    private int item_WIDTH = 100;
+    private int item_HEIGHT = 100;
+
+
+
+    public Item(Sprite sprite,int id, String type) {
+        super(sprite);
+        this.image = sprite;
+        this.type = type;
+        this.id = id;
+        this.type = type;
+
+        initialize();
+    }
+
+    /*
+    set default value of item attributes
+     */
+    private void initialize() {
+
+        this.setSize(item_WIDTH,item_HEIGHT);
+        this.setPosition(Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()/2);
+    }
+
+
+    @Override
+    public void draw(Batch batch) {
+        update(Gdx.graphics.getDeltaTime());
+        super.draw(batch);
+    }
+
+
+
+    /*
+    update item status and response to events
+    react on collision
+  
+     */
+
+    public void update(float delta)
+    {
+
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Sprite getImage() {
+        return image;
+    }
+
+    public void setImage(Sprite image) {
+        this.image = image;
+    }
 }
