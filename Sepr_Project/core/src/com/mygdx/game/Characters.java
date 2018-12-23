6468ab9b3b17b5647ab8581a9e7a0b6b08624042
eddiebattman <eddie_batt@hotmail.com>
2018-12-23 17:26:21 +0000

    Added checkBounds

--- Sepr_Project/core/src/com/mygdx/game/Characters.java
+++ Sepr_Project/core/src/com/mygdx/game/Characters.java
@@ -53,4 +53,20 @@ public class Characters {
 		int height = Math.round((hitBoxDim[3]- hitBoxDim[2])*scale);
 		return height;
 	}
+	//Makes sure sprite is within the current screen
+	public void checkBounds(Sprite screen) {
+		if(sprite.getX() + hitBoxDim[0]*scale < screen.getX()) {
+        	sprite.setPosition(screen.getX()-hitBoxDim[0]*scale , sprite.getY());
+        }
+		if((sprite.getX() + hitBoxDim[1]*scale) > (screen.getX()+screen.getWidth())) {
+        	sprite.setPosition(screen.getX()+screen.getWidth() - hitBoxDim[1]*scale, sprite.getY());
+        }
+		if(sprite.getY() + hitBoxDim[3]*scale > (screen.getY() + screen.getHeight())) {
+            sprite.setPosition(sprite.getX(), (screen.getY() + screen.getHeight()- hitBoxDim[3]*scale));	
+        }
+		if(sprite.getY()+ hitBoxDim[2]*scale < screen.getY()) {
+           	sprite.setPosition(sprite.getX(), screen.getY() - hitBoxDim[2] * scale);
+         }
+		
+	}
 }
