1c4b8a038d8e7cad79a45e7aff564df3552c87b8
aa1764 <44054100+aa1764@users.noreply.github.com>
2019-01-14 20:20:23 +0000

    Update PowerUp.java

--- Sepr_Project/Sepr_Project/core/src/com/mygdx/game/PowerUp.java
+++ Sepr_Project/Sepr_Project/core/src/com/mygdx/game/PowerUp.java
@@ -1,5 +1,35 @@
 package com.mygdx.game;
 
-public class PowerUp {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+/*
+ * Powerups class. 3 different types are currently implemented. 
+ */
+public class PowerUps extends Item {
+
+	public PowerUps(Sprite sprite, String type) {
+		super(sprite, type);
+	}
+	
+	/*
+	 * Applies the effect based on the PowerUp type.
+	 * @param Player
+	 * /
+
+	public void applyEffect(Player currentPlayer) {
+
+		if (this.getType() == "HEALTH_REGEN") {
+			currentPlayer.resetHealth();
+		} else if (this.getType() == "DECREASE_DAMAGE") {
+			currentPlayer.setDamage(currentPlayer.getDamage() - 2);
+		} else if (this.getType() == "INCREASE_SPEED") {
+			currentPlayer.setSpeed(10);
+		} else {
+
+			System.out.println("Power up type does not match any known type");
+		}
+	}
 
 }
