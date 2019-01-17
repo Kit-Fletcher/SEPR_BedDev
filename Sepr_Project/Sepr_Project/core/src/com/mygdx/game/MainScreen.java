15cd7882f6c88ac280e393d1f909ae937e8c39b5
Kit_F <fletcherkitcat@gmail.com>
2019-01-17 02:10:13 +0000

    Adding main menu code

--- Sepr_Project/Sepr_Project/core/src/com/mygdx/game/MainScreen.java
+++ Sepr_Project/Sepr_Project/core/src/com/mygdx/game/MainScreen.java
@@ -1,6 +1,8 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /*
@@ -9,13 +11,97 @@ This allows the application to easily have multiple screens.
  */
 
 public class MainScreen extends Game {
+	
 
-    SpriteBatch batch;
+	  Texture background;
 
-    public void create() {
-        batch = new SpriteBatch();
-        this.setScreen(new GameScreen(this));
-    }
+	  Texture startButtonActive;
+	  Texture startButtonInActive;
+	  Texture loadButtonActive;
+	  Texture loadButtonInActive;
+	  Texture quitButtonActive;
+	  Texture quitButtonInActive;
+
+	  //standard values assuming a 21:9 ratio of
+	  //the button sizes
+	  private static final int btnWidth = 210;
+	  private static final int btnHeight = 90;
+
+	  private static final int loadX = 100;
+	  private static final int loadY = 100;
+	  private static final int startX = 100;
+	  private static final int startY = 200;
+	  private static final int quitX = 100;
+	  private static final int quitY = 300;
+
+	  SpriteBatch batch;
+
+	    public void create() {
+	        batch = new SpriteBatch();
+	        //this.setScreen(new GameScreen(this));
+	    }
+	  
+	  //SEPRGame game is a place holder for a
+	  //game object for the main game
+	  public MainScreen () {
+	      background = new Texture("backgroundSEPR.png");
+	      startButtonActive = new Texture("startActivated.png");
+	      startButtonInActive = new Texture("startUnactivated.png");
+	      loadButtonActive = new Texture("loadActivated.png");
+	      loadButtonInActive = new Texture("loadUnactivated.png");
+	      quitButtonActive = new Texture("quitActivated.png");
+	      quitButtonInActive = new Texture("quitUnactivated.png");
+
+	  }
+
+	 
+	  public void render(float delta) {
+
+	      batch.begin();
+
+	    
+	    batch.draw(background, 0, 0, 500, 500);
+	    
+	    int x = Gdx.input.getX();
+	    int y = 500 - Gdx.input.getY();
+	    
+	    //decides if the mouse is within button region
+	    //if is within region, changes button to active state
+	    if (x < (startX + btnWidth) && x > startX &&
+	            y < (startY + btnHeight) && y > startY)
+	    {
+	      batch.draw(startButtonActive, startX, startY, btnWidth, btnHeight);
+	    } else {
+	      batch.draw(startButtonInActive, startX, startY, btnWidth, btnHeight);
+	    }
+
+
+	    if (x < (loadX + btnWidth) && x > loadX &&
+	            y < (loadY + btnHeight) && y > loadY)
+	    {
+	      batch.draw(loadButtonActive, loadX, loadY, btnWidth, btnHeight);
+	    } else {
+	      batch.draw(loadButtonInActive, loadX, loadY, btnWidth, btnHeight);
+	    }
+
+
+	    if (x < (quitX + btnWidth) && x > quitX &&
+	            y < (quitY + btnHeight) && y > quitY)
+	    {
+	      batch.draw(quitButtonActive, quitX, quitY, btnWidth, btnHeight);
+	    } else {
+	      batch.draw(quitButtonInActive, quitX, quitY, btnWidth, btnHeight);
+	    }
+
+	    batch.end();
+
+	  }
+
+	
+	
+	
+	
+    
 
     public void render() {
         super.render();
